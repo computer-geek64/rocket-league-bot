d99c9d487fe0e610c6ea0dbec19fa2abe66ebcd2
Ashish D'Souza <ashish.dsouza@students.polytech.k12.de.us>
2018-09-09 14:42:29 -0400

    Update Encryption.py

--- unix/Encryption.py
+++ unix/Encryption.py
@@ -1,6 +1,6 @@
 # Encryption.py
 # Ashish D'Souza
-# September 8th, 2018
+# September 9th, 2018
 
 from Crypto.Cipher import AES
 import base64
@@ -8,11 +8,11 @@ import base64
 
 def encrypt(text, passphrase):
     IV = "1234567890123456"
-    aes = AES.new(passphrase, AES.MODE_CFB, IV)
-    return base64.b64encode(aes.encrypt(text)).decode("ascii")
+    aes = AES.new(str.encode(passphrase), AES.MODE_CFB, str.encode(IV))
+    return base64.b64encode(aes.encrypt(str.encode(text))).decode("ascii")
 
 
 def decrypt(cipher_text, passphrase):
     IV = "1234567890123456"
-    aes = AES.new(passphrase, AES.MODE_CFB, IV)
-    return aes.decrypt(base64.b64decode(cipher_text)).decode("ascii")
+    aes = AES.new(str.encode(passphrase), AES.MODE_CFB, str.encode(IV))
+return aes.decrypt(base64.b64decode(cipher_text)).decode("ascii")
