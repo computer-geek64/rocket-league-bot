08a5fe7f2bb0f1a41198f3c88df81cd37c4fe8e9
Ashish D'Souza <ashish.dsouza@students.polytech.k12.de.us>
2018-09-09 19:02:57 -0400

    Update Main.py

--- win/Main.py
+++ win/Main.py
@@ -7,7 +7,7 @@ import os
 
 
 while True:
+    sleep(15 * 60)
     os.system("taskkill /IM chrome.exe /F")
     os.system("taskkill /IM chromedriver.exe /F")
     os.system("python Bot.py")
-    sleep(15 * 60)
