7240e30c03553b157d2add515489e46757f2a5f6
computer-geek64 <ashish.dsouza@students.polytech.k12.de.us>
2018-09-09 18:40:28 -0400

    Update

--- win/Main.py
+++ win/Main.py
@@ -1,25 +1,13 @@
 # Main.py
 # Ashish D'Souza
-# September 8th, 2018
+# September 9th, 2018
 
 from time import sleep
-from Encryption import *
 import os
 
 
-password = ((open(os.getcwd().split("/win")[0] + "/password.txt", "r").readlines()[0].strip()) * 16)[:16]
-
-
-def read():
-    with open(os.getcwd().split("/win")[0] + "/settings.txt", "r") as file:
-        lines = file.readlines()
-        file.close()
-    lines = list(map(lambda x: decrypt(x, password), lines))
-    return int(lines[0].strip())
-
-
 while True:
-    sleep(read() * 60)
     os.system("taskkill /IM chrome.exe /F")
     os.system("taskkill /IM chromedriver.exe /F")
     os.system("python Bot.py")
+    sleep(15 * 60)
