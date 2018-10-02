1cbb39afa364497d1bfdaf1fcaf87bd638643aea
Ashish D'Souza <ashish.dsouza@students.polytech.k12.de.us>
2018-10-02 17:51:39 -0400

    Update Main.py

--- win/Main.py
+++ win/Main.py
@@ -10,7 +10,7 @@ while True:
     print("[-] Resetting")
     sleep(15 * 60)
     print("[+] Successfully reset.")
-    print("[-] Starting bot...")
+    print("[+] Running bot...")
     os.system("taskkill /IM chrome.exe /F")
     os.system("taskkill /IM chromedriver.exe /F")
     os.system("python Bot.py")
